95a7a4771ebcf0c6773aac67e4707949297fa070
Siro01 <sirooo444@gmail.com>
2024-09-05 21:18:35 -0300

    Clase 03 Python
    Sistemas Numéricos y String-> Parte 2

---
+++ SiroTorres/LaboratorioIV/Python/Clase01/tipo_bool.py
@@ -0,0 +1,61 @@
+# Bool contiene los valores True y False
+# Los tipos numericos, el flase para el 0 (cero), true para los demas valores
+valor = 0.0
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 0.1
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+#Tipo String -> False '', True demas valores
+valor = ''
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 'Hola'
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+#Tipo colecciones -> False para colecciones vacias
+#Tipo colecciones -> True para todas las demas
+# Lista
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacia: {valor}, Resultado: {resultado}')
+
+valor = [2,3,4]
+resultado = bool(valor)
+print(f'valor de una lista con elementos: {valor}, Resultado: {resultado}')
+
+# Tupla
+valor = ()
+resultado = bool(valor)
+print(f'valor de una tupla vacia: {valor}, Resultado: {resultado}')
+
+valor = (5,)
+resultado = bool(valor)
+print(f'valor de una tupla con elementos: {valor}, Resultado: {resultado}')
+
+#Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacio: {valor}, Resultado: {resultado}')
+
+valor = {'Nombre': 'Juan', 'Apellido': 'Perez'}
+resultado = bool(valor)
+print(f'valor de un diccionario con elementos: {valor}, Resultado: {resultado}')
+
+# Sentencias de control con bool
+if (1,):
+    print('Regresa verdadero')
+else:
+    print('Regresa falso')
+
+# Ciclos
+variable = 11
+while variable:
+    print('Regresa verdadero')
+    break
+else:
+    print('Regresa falso')
