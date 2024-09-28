983ff0b07a06ca3718a66fe37b2c6f48607796d0
Emilia <135785905+MaEmiliaB@users.noreply.github.com>
2024-09-28 17:52:32 -0300

    Clase 2 Python completa

---
+++ Emilia Bazan/Programación IV/Python/Clase 1/sistemas_numeracion.py
@@ -0,0 +1,33 @@
+# Profundizando en los sistemas de numeración
+
+# Sistema decimal
+a = 10
+print(f"a decimal: {a}")
+
+# Sistema binario
+a = 0b1010
+print(f"a binaario: {a}")
+
+# Sistema octal
+a = 0o12
+print(f"a octal: {a}")
+
+# Sistema hexadecimal
+a = 0xA
+print(f"a hexadecimal: {a}")
+
+# Base decimal
+a = int("23", 10)
+print(f"a base decimal: {a}")
+
+# Base binario
+a = int("10111", 2)
+print(f"a base binario: {a}")
+
+# Base octal
+a = int("27", 8)
+print(f"a base octal: {a}")
+
+# Base hexadecimal
+a = int("17", 16)
+print(f"a base hexadecimal: {a}")
