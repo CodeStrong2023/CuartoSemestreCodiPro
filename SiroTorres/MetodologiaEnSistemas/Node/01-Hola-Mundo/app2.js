c4c49a1eb03b83a238d0c861d5dafdac8926680d
Siro01 <sirooo444@gmail.com>
2024-10-11 13:32:16 -0300

    Clase 03 Node
    Comenzamos con pruebas en ejecución

---
+++ SiroTorres/MetodologiaEnSistemas/Node/01-Hola-Mundo/app2.js
@@ -0,0 +1,6 @@
+const saludar = (nombre) =>{ //Funcion de flecha
+    return `Saludos de ${nombre}`; //Usamos back ticks`s
+} //Esto es igual a :'Saludos' + nombre;
+console.log(saludar('Ariel'));
+
+
