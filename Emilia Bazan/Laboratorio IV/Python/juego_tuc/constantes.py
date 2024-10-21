0ba1c67f9ba02e68362503d3e87c5510ed1b304c
Emilia Bazan <e.bazanazargado@gmail.com>
2024-10-21 17:29:02 -0300

    Clase 8 lab py completa

---
+++ Emilia Bazan/Laboratorio IV/Python/juego_tuc/constantes.py
@@ -0,0 +1,17 @@
+# Constantes.py
+
+# Dimensiones de la pantalla
+
+screen_width: 1700
+screen_heigth: 1000
+
+# ruta de los assets
+
+ASSETS_PATH = "venv\assets"
+
+# Ruta de los archivos
+imperial_march_path = f'{
+    ASSETS_PATH}, venv\assets\sounds\Imperial March - Kenobi.mp3'
+start_image_path = f'{ASSETS_PATH}/image/inicio/start.png'
+estrella_path = f'{ASSETS_PATH}/image/inicio/estrella.png'
+fondo1_path = f'{ASSETS_PATH}/image/fondo1.jpg.png'
